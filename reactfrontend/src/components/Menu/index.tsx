605996d8475a6616929c049694615803f4d46173
AntonioNigro1 <46200602+AntonioNigro1@users.noreply.github.com>
2022-08-18 12:26:33 -0300

    Components loading fixed
    fixed issues when loading components

--- reactfrontend/src/components/Menu/index.tsx
+++ reactfrontend/src/components/Menu/index.tsx
@@ -4,27 +4,29 @@ import { ComponentClass, useState } from 'react';
 import { Register } from '../Register/index';
 import { Login } from '../Login';
 
+import { BrowserRouter, Link } from 'react-router-dom'
 export default function Menu() {
-  const [component, setComponent] = useState<React.ComponentClass>();
-
-  const LoadComponent = async (component: ComponentClass) => {
-    setComponent(component);
-  }
 
   return (
     <>
       <div className='menu'>
+        <img className='top_img' src="/images/topbg.jpg" alt='' />
 
         <button className='menu_button'>Deposit</button>
+
         <button className='menu_button'>Withdraw</button>
+
         <button className='menu_button'>Exchange</button>
+
         <button className='menu_button'>Trade</button>
+
         <button className='menu_button'>History</button>
-        <button className='menu_button'>Login</button>
-        <button className='menu_button'>Register</button>
+        <Link to="/Login">
+          <button className='menu_button'>Login</button></Link>
+        <Link to="/Register">
+          <button className='menu_button'>Register</button></Link>
       </div>
-      {component}
+
     </>
   )
 }
-
